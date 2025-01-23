27a5470df570b80d13fd8756f46ec9009adb3fc9
Alex Sizov <alex_sizow@outlook.com>
2025-01-23 18:07:45 +0300

    add strapi integration in nuxt

--- packages/frontend/nuxt.config.ts
+++ packages/frontend/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@vueuse/nuxt', '@nuxt/fonts'],
+  modules: ['@vueuse/nuxt', '@nuxt/fonts', '@nuxtjs/strapi'],
   fonts: {
     families: [
       { name: 'Roboto', src: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap' }
