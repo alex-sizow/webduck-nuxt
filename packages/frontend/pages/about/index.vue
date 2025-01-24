8c90992cfb3220f86e4b769ba1ff3c8b8420cf54
Alex Sizov <alex_sizow@outlook.com>
2025-01-24 11:57:58 +0300

    add markdown

--- packages/frontend/pages/about/index.vue
+++ packages/frontend/pages/about/index.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { marked } from 'marked';
+
 interface AboutData {
   title: string;
   Main: string;
@@ -8,12 +10,13 @@ const { findOne } = useStrapi();
 
 const response = await findOne<AboutData>('about');
 const aboutData = response.data;
+const markedDataMain = marked(aboutData.Main);
 </script>
 
 <template>
   <div>
     <h1>{{ aboutData.title }}</h1>
-    <main>{{ aboutData.Main }}</main>
+    <main v-html="markedDataMain"></main>
     <p></p>
   </div>
 </template>
